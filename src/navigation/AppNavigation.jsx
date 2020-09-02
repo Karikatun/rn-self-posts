852f9ce6ad10a4d563ba1d70367b372f1e81d2de
Shevchenko Anton <shevchenko-anton2014@yandex.ru>
2020-09-02 18:51:20 +0300

    Добавление навигации

---
+++ src/navigation/AppNavigation.jsx
@@ -0,0 +1,8 @@
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from '@react-navigation/stack'
+
+const PostNavigator = createStackNavigator({
+
+})
+
+export const AppNavigation = createAppContainer(PostNavigator)
